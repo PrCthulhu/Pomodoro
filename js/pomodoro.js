5636da119bb0df46e7a329d8316132813f3b04fa
Arthur Hamon <arthur.hamon@etu.unicaen.com>
2023-09-19 17:50:24 +0200

    final commit v2 + comments

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -2,75 +2,73 @@
 class Timer {
     constructor([workTime, breakTime]) {
         // Initialize timer properties.
-        this.counter = workTime;
-        this.seconds = 0;
-        this.minutes = 0;
-        this.stage = [workTime, breakTime];
-        this.index = 0;
-        this.isRunning = false;
+        this.counter = workTime; // Initialize the timer to the work time.
+        this.seconds = 0; // Initialize seconds.
+        this.minutes = 0; // Initialize minutes.
+        this.stage = [workTime, breakTime]; // Store work and break times.
+        this.index = 0; // Initialize the stage index (0 for work, 1 for break).
+        this.isRunning = false; // Initialize the timer as not running.
     }
 
     // Function to switch between work and break times.
     nextIndex = () => {
-        this.index = (this.index + 1) % 2;
+        this.index = (this.index + 1) % 2; // Toggle between 0 and 1 (work and break).
         console.log(this.index);
-        this.counter = this.stage[this.index];
+        this.counter = this.stage[this.index]; // Set the timer to the current stage.
         console.log(this.counter);
-        this.start();
+        this.start(); // Start the timer.
 
-        // Update UI based on the current stage (work or break).
+        // Update the UI based on the current stage (work or break).
         if (this.index === 0) {
-            document.getElementById("status").textContent = "workTime"
-            document.getElementById('body').style.backgroundColor = "rgba(184, 104, 144)"
-            document.getElementById('buttonStart').style.backgroundColor = "rgba(184, 104, 144)"
-            document.getElementById('status').style.backgroundColor = "rgb(189, 35, 176)"
+            document.getElementById("status").textContent = "workTime";
+            document.getElementById('body').style.backgroundColor = "rgba(184, 104, 144)";
+            document.getElementById('buttonStart').style.backgroundColor = "rgba(184, 104, 144)";
+            document.getElementById('status').style.backgroundColor = "rgb(189, 35, 176)";
         } else {
-            document.getElementById("status").textContent = "breakTime"
-            document.getElementById('body').style.backgroundColor = "rgba(104, 139, 184)"
-            document.getElementById('buttonStart').style.backgroundColor = "rgba(104, 139, 184)"
-            document.getElementById('status').style.backgroundColor = "rgb(35, 102, 189)"
+            document.getElementById("status").textContent = "breakTime";
+            document.getElementById('body').style.backgroundColor = "rgba(104, 139, 184)";
+            document.getElementById('buttonStart').style.backgroundColor = "rgba(104, 139, 184)";
+            document.getElementById('status').style.backgroundColor = "rgb(35, 102, 189)";
         }
     }
 
     // Function to decrement the timer by one second.
     addSecond = () => {
-        this.counter -= 1;
-        this.seconds = this.counter % 60;
-        this.minutes = parseInt(this.counter / 60);
+        this.counter -= 1; // Decrease the counter by 1 second.
+        this.seconds = this.counter % 60; // Calculate remaining seconds.
+        this.minutes = parseInt(this.counter / 60); // Calculate remaining minutes.
         document.getElementById("timer").innerHTML = `${this.minutes.toString().padStart(2, '0')}:${this.seconds.toString().padStart(2, '0')}`;
 
         // If the timer reaches zero, stop the interval and move to the next stage.
         if (this.counter <= 0) {
-            clearInterval(this.intervalId);
-            this.nextIndex();
+            clearInterval(this.intervalId); // Stop the timer interval.
+            this.nextIndex(); // Move to the next stage.
         }
     };
 
     // Function to start the timer.
     start = () => {
-        clearInterval(this.intervalId);
-        this.isRunning = true;
-        this.intervalId = setInterval(this.addSecond, 1000);
+        clearInterval(this.intervalId); // Clear any existing timer intervals.
+        this.isRunning = true; // Set the timer as running.
+        this.intervalId = setInterval(this.addSecond, 1000); // Start the timer interval.
     };
 
     // Function to reset the timer.
     reset = () => {
-        location.reload();
+        location.reload(); // Reload the page to reset the timer.
     }
 }
 
 // Get the button element with the ID 'buttonStart'.
 const bouttonStart = document.getElementById('buttonStart');
 
-let isStarted = false
+let isStarted = false;
 
 // Add a click event listener to the 'Start' button.
 bouttonStart.addEventListener('click', () => {
     document.getElementById("buttonStart").innerHTML = '<em class="fa-solid fa-rotate-right"></em>';
 
-
-
-    // Set initial work and break times.
+    // Get initial work and break times from input fields.
     let workTime = document.getElementById("timeOfWork").value;
     let breakTime = document.getElementById("timeOfBreak").value;
     let tempsDeTravail = [workTime * 60, breakTime * 60];
@@ -83,7 +81,7 @@ bouttonStart.addEventListener('click', () => {
         location.reload();
     }
 
-    isStarted = true
+    isStarted = true;
 
     // Start the timer.
     chrono.start();
@@ -95,18 +93,16 @@ let minutes = parseInt(document.getElementById("timeOfWork").value);
 let timerLoad = document.getElementById("timer");
 timerLoad.innerHTML = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 
+// Add an event listener to update the timer when the 'timeOfWork' input changes.
 document.getElementById("timeOfWork").addEventListener('input', () => {
     let seconds = (document.getElementById("timeOfWork").value * 60) % 60;
     let minutes = parseInt(document.getElementById("timeOfWork").value);
     let timerLoad = document.getElementById("timer");
     timerLoad.innerHTML = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-})
+});
 
+// Add an event listener for the 'timeOfBreak' input (currently missing functionality).
 document.getElementById("timeOfBreak").addEventListener('input', () => {
-    let seconds = (document.getElementById("timeOfBreak").value * 60) % 60;
-    let minutes = parseInt(document.getElementById("timeOfBreak").value);
-})
-
-
-
-
+    seconds = (document.getElementById("timeOfBreak").value * 60) % 60;
+    minutes = parseInt(document.getElementById("timeOfBreak").value);
+});
